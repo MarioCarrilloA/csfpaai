130c7486b3f5b44fcca7dd75a1f421cbbb6325c8
mario carrillo <carrillo@rhrk.uni-kl.de>
2021-06-02 23:59:29 +0200

    Compute second iteration
    This change allows to compute CIFAR after applying a tranformation
    to the dataset. This transformation consists of cropping the 5% of
    the area with highest values detected by CAM.
    
    Signed-off-by: mario carrillo <carrillo@rhrk.uni-kl.de>

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ test_transform = transforms.Compose([
         transforms.ToTensor(),
 ])
 
-epochs=5
+epochs=50
 
 # Download & transform CIFAR-10 datasets
 train_full_dataset = datasets.CIFAR10("./data", train=True,
@@ -36,3 +36,34 @@ test_loader = torch.utils.data.DataLoader(test_dataset,
 # Train model and collect data
 model_base, pct_correct, pct_classes, trainl, testl = build_model(train_loader, test_loader, epochs)
 collect_results(epochs, pct_correct, trainl, testl, pct_classes, out_filename)
+
+#########################################################################################################
+# SECOND ATTEMP
+#########################################################################################################
+crop_transformation_a1 = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),#########################
+    transforms.RandomHorizontalFlip(),##################################
+    #transforms.Lambda(lambda x: crop_preprocess(x, model_base)),
+    transforms.ToTensor(),##############################
+    transforms.Lambda(lambda x: crop_preprocess(x, model_base)),####################
+])
+
+train_full_dataset_a1 = datasets.CIFAR10("./data", train=True,
+                                 transform=crop_transformation_a1, download=True)
+
+test_dataset_a1 = datasets.CIFAR10("./data", train=False,
+                                transform=test_transform, download=True)
+
+# Split datasets
+train_num_samples_a1 = int(len(train_full_dataset_a1) * 0.9) # 90%(Training set).
+val_num_samples_a1 = int(len(train_full_dataset_a1) * 0.1)   #10%(Validation set)
+train_dataset_a1, validation_dataset_a1 = random_split(train_full_dataset_a1, [train_num_samples_a1, val_num_samples_a1])
+
+# Loaders
+train_loader_a1 = torch.utils.data.DataLoader(train_full_dataset_a1,
+                                           batch_size=128, shuffle=True)
+test_loader_a1 = torch.utils.data.DataLoader(test_dataset_a1,
+                                          batch_size=5000, shuffle=False)
+
+model_a1, pct_correct_a1, pct_classes_a1, trainl, testl = build_model(train_loader_a1, test_loader_a1, epochs)
+collect_results(epochs, pct_correct_a1, trainl, testl, pct_classes_a1, out_filename)
