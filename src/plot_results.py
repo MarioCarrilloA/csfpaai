2966d4bc86ddbbe9b527d1882ffa6cbd60fcf04e
mario carrillo <carrillo@rhrk.uni-kl.de>
2021-06-17 18:51:35 +0200

    Add support to plot loss data
    Signed-off-by: mario carrillo <carrillo@rhrk.uni-kl.de>

--- src/plot_results.py
+++ src/plot_results.py
@@ -1,6 +1,8 @@
 import os.path
 import json
 import matplotlib.pyplot as plt
+import sys
+import random
 
 # Classes labels CIFAR-10
 classes = ('plane',
@@ -13,24 +15,9 @@ classes = ('plane',
            'horse',
            'ship',
            'truck')
+out_path = "charts/"
 
-def plot_CIFAR10_results():
-    #TODO: Optimze this code
-    out_path = "charts/"
-    if os.path.isdir(out_path) == False:
-        os.makedirs(out_path)
-
-    file_name = 'results.json'
-    data_file = []
-    if os.path.isfile(file_name) == True:
-        with open(file_name, 'r') as json_file:
-            data_file = json.load(json_file)
-            result_id = len(data_file) + 1
-            json_file.close()
-    else:
-        print("The file {} does not exist".format(file_name))
-
-    # Plot classes comparison
+def plot_classes_results(data_file):
     for c in range(len(classes)):
         tmp = []
         x =[]
@@ -40,25 +27,80 @@ def plot_CIFAR10_results():
             tmp.append(r["classes_pcts"][c])
             x.append("i" + str(i))
         plt.clf()
-        plt.title("Class accuracy: " + classes[c])
+        plt.title("Class accuracy: " + classes[c], fontsize=15)
         plt.ylim([0,100])
-        plt.bar(x, tmp,  align='center', color='blue', width=0.4)
+        rgb = (random.random(), random.random(), random.random())
+        plt.bar(x, tmp,  align='center', color=[rgb], width=0.4)
         plt.ylabel("Percentage")
         plt.xlabel("Iterations")
+
+        # Labels above bars
+        for i, v in enumerate(tmp):
+            plt.text(i - 0.20, v + 1, str(round(v, 2)) + "%")
         plt.savefig(out_path + classes[c]+".jpg", bbox_inches='tight')
-    ###################################################################
-    # Plot General accuracy
-    ##################################################################
+
+
+def plot_model_accuracy(data_file):
     acc = []
+    x =[]
+    i = 0
     for r in data_file:
-        acc = r["accuracy"]
+        i+=1
+        acc.append(r["accuracy"])
+        x.append("i" + str(i))
     plt.clf()
     plt.title("Model accuracy per iteration")
     plt.ylim([0,100])
     plt.bar(x, acc,  align='center', color='green', width=0.4)
     plt.ylabel("Percentage")
     plt.xlabel("Iterations")
+
+    # Labels above bars
+    for i, v in enumerate(acc):
+        plt.text(i - 0.20, v + 1, str(round(v, 2)) + "%")
     plt.savefig(out_path + "model_accuracy.jpg", bbox_inches='tight')
 
 
-plot_CIFAR10_results()
+def plot_loss(data_file):
+    i = 0
+    for r in data_file:
+        img_name = "loss_iteration_{}.png".format(i)
+        train_loss = r['train_loss']
+        test_loss = r['test_loss']
+        epochs = len(train_loss)
+        plt.clf()
+        plt.title("Train and test loss - Iteration {}".format(i))
+        plt.plot(train_loss, color='red', label='Train loss')
+        plt.plot(test_loss, color='blue', label='Test loss')
+        plt.ylabel("Loss")
+        plt.xlabel("Epochs")
+        plt.xlim([0, epochs])
+        plt.savefig(out_path + img_name, bbox_inches='tight')
+        i+=1
+
+def main():
+    if len(sys.argv) <= 1:
+        print("error: no input file")
+        sys.exit(0)
+
+    input_file = sys.argv[1]
+    if os.path.isdir(out_path) == False:
+        os.makedirs(out_path)
+
+    data_file = []
+    if os.path.isfile(input_file) == True:
+        with open(input_file, 'r') as json_file:
+            data_file = json.load(json_file)
+            result_id = len(data_file) + 1
+            json_file.close()
+    else:
+        print("The file {} does not exist".format(input_file))
+
+    print("plotting...")
+    plot_model_accuracy(data_file)
+    plot_classes_results(data_file)
+    plot_loss(data_file)
+    print("Done!")
+
+if __name__ == "__main__":
+    main()
