924aef84ad7965042b93bab092e268adc29fb26e
Mario Carrillo <carrillo@rhrk.uni-kl.de>
2021-07-17 15:00:33 +0200

    Remove unused modules
    These modules are not used anymore at this point of the
    project.
    
    Signed-off-by: Mario Carrillo <carrillo@rhrk.uni-kl.de>

--- src/common.py
+++ src/common.py
@@ -1,23 +1,15 @@
-import csv
-import datadings
 import json
 import io
 import matplotlib.pyplot as plt
 import numpy as np
-import os
 import os.path
-import pandas as pd
 import PIL
 import random
-import shutil
-import simplejpeg as sjpg
-import skimage.transform
 import sys
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-import torchvision
 
 from datadings.sets import ImageClassificationData
 from datadings.writer import FileWriter
@@ -30,9 +22,7 @@ from sacred.observers import file_storage
 from sklearn.model_selection import train_test_split
 from torch import topk
 from torch.autograd import Variable
-from torch.utils.data import random_split
-from torch.utils.data import Dataset
-from torch.utils.data import Subset
+from torch.utils.data import random_split, Dataset, Subset
 from torchvision import models, datasets, transforms
 from torchvision.utils import save_image
 from tqdm import tqdm
