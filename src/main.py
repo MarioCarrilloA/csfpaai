df1872f3b4f3a4c904156b01dedb8bd914db4b07
Mario Carrillo <carrillo@rhrk.uni-kl.de>
2021-07-18 19:22:25 +0200

    Organize code
    This change just orginzes a little the code and add a pair
    of notes to give more readability.
    
    Signed-off-by: Mario Carrillo <carrillo@rhrk.uni-kl.de>

--- src/main.py
+++ src/main.py
@@ -57,7 +57,7 @@ def main(
         out_img = "../res/dataset_samples_{}.png".format(itr)
         ext_out_img = "../res/ctestds_dataset_samples_{}.png".format(itr)
 
-        # Compute base
+        # Compute base when our iteration is 0
         if itr == 0:
             print("Compute base model")
             train_loader = torch.utils.data.DataLoader(
@@ -70,6 +70,7 @@ def main(
                             batch_size=5000,
                             shuffle=False
             )
+
             test_transformed_loader = test_loader
             # Train model
             prev_model, metrics = build_model(
@@ -86,93 +87,105 @@ def main(
             save_dataset_samples(test_transformed_loader, ext_out_img)
             continue
 
-        # When the base model has been trained.
-        ######################################################################
-        extractor = CAM(prev_model, 'resnet.layer4', 'resnet.fc')
-        crop_transformation = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Lambda(lambda x: crop_preprocess(x, prev_model,
-                    extractor, cropped_pixels))])
 
-        save_random_samples(
-                prev_model,
-                extractor,
-                num_samples,
-                crop_transformation,
-                train_dataset,
-                itr
-        )
-
-        save_sequential_samples(
-                prev_model,
-                extractor,
-                num_samples,
-                crop_transformation,
-                test_transformed_loader,
-                itr
-        )
-
-
-        # This line is to avoid saving the images with the transformations
-        # used for data augmentation.
-        if isinstance(train_dataset, croppedCIFAR10):
-            train_dataset.transform = None
+        # When the base model has been trained and our iteration is
+        # greater than 0 we are going to work with cropped data.
         else:
-            train_dataset.dataset.transform = None
+            # Set algorithm to compute  class-specific activation of
+            # convolutional layers.
+            extractor = CAM(prev_model, 'resnet.layer4', 'resnet.fc')
 
-        # Prepare dataset and loaders for new iteration
-        print("Create new TRAIN dataset...")
-        create_new_dataset(
-                train_dataset,
-                new_dataset_dir,
-                crop_transformation
-        )
-        print("Creating new TEST dataset")
-        test_transformed_dataset.transform = None
-        create_new_dataset(
-                test_transformed_dataset,
-                new_dataset_dir,
-                crop_transformation,
-                train=False
-        )
-        test_transformed_dataset.transform = transforms.ToTensor()
+            # Use the class activation map algorithm and the previous
+            # trained model to specify the transformation for next iteration.
+            crop_transformation = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Lambda(lambda x: crop_preprocess(x, prev_model,
+                        extractor, cropped_pixels))])
+
+            # Save some samples using the our customized tranformation
+            save_random_samples(
+                    prev_model,
+                    extractor,
+                    num_samples,
+                    crop_transformation,
+                    train_dataset,
+                    itr
+            )
+
+            save_sequential_samples(
+                    prev_model,
+                    extractor,
+                    num_samples,
+                    crop_transformation,
+                    test_transformed_loader,
+                    itr
+            )
 
 
-        train_dataset = croppedCIFAR10(
-                        root=new_dataset_dir,
-                        transform=train_transform
-        )
-        train_loader = torch.utils.data.DataLoader(
-                        train_dataset,
-                        batch_size=128,
-                        shuffle=True
-        )
+            # This line is to avoid saving the images with the transformations
+            # used for data augmentation.
+            if isinstance(train_dataset, croppedCIFAR10):
+                train_dataset.transform = None
+            else:
+                train_dataset.dataset.transform = None
 
-        test_transformed_dataset = croppedCIFAR10(
-                        root=new_dataset_dir,
-                        transform=test_transform,
-                        train=False
-        )
-        test_transformed_loader = torch.utils.data.DataLoader(
-                        test_transformed_dataset,
-                        batch_size=5000,
-                        shuffle=False
-        )
+            # Prepare dataset and loaders for new iteration
+            print("Create new TRAIN dataset...")
+            create_new_dataset(
+                    train_dataset,
+                    new_dataset_dir,
+                    crop_transformation
+            )
+            print("Creating new TEST dataset")
+            test_transformed_dataset.transform = None
+            create_new_dataset(
+                    test_transformed_dataset,
+                    new_dataset_dir,
+                    crop_transformation,
+                    train=False
+            )
 
-        prev_model, metrics = build_model(
-                            train_loader,
-                            test_loader,
-                            epochs,
-                            learning_rate,
-                            test_transformed_loader,
-        )
+            # Remove extra transformations and only allow tensor conversion
+            test_transformed_dataset.transform = transforms.ToTensor()
 
-        #_run.log_scalar(1, metrics)
-        avg_cpix = sum(cropped_pixels) / len(cropped_pixels)
-        metrics.update({'avg_cropped_pixels': avg_cpix})
-        collect_results(metrics, out_filename)
-        save_dataset_samples(train_loader, out_img)
-        save_dataset_samples(test_transformed_loader, ext_out_img)
+            # Create datasets with the cropped data obtained from
+            # previous iteration.
+            train_dataset = croppedCIFAR10(
+                            root=new_dataset_dir,
+                            transform=train_transform
+            )
+            train_loader = torch.utils.data.DataLoader(
+                            train_dataset,
+                            batch_size=128,
+                            shuffle=True
+            )
 
-        if all(vp <= min_accuracy for vp in metrics['testds_classes_pcts']):
-            break
+            test_transformed_dataset = croppedCIFAR10(
+                            root=new_dataset_dir,
+                            transform=test_transform,
+                            train=False
+            )
+            test_transformed_loader = torch.utils.data.DataLoader(
+                            test_transformed_dataset,
+                            batch_size=5000,
+                            shuffle=False
+            )
+
+            prev_model = None
+            prev_model, metrics = build_model(
+                                train_loader,
+                                test_loader,
+                                epochs,
+                                learning_rate,
+                                test_transformed_loader,
+            )
+
+            #_run.log_scalar(1, metrics)
+            avg_cpix = sum(cropped_pixels) / len(cropped_pixels)
+            metrics.update({'avg_cropped_pixels': avg_cpix})
+            collect_results(metrics, out_filename)
+            save_dataset_samples(train_loader, out_img)
+            save_dataset_samples(test_transformed_loader, ext_out_img)
+
+            if all(vp <= min_accuracy for vp in metrics['testds_classes_pcts']):
+                break
